b0a67f55ad133a69b8c730f4b8ba124822e71eeb
jureuk7 <jureuk7@protonmail.com>
2024-10-12 01:44:21 +0900

    feat: Add ColorPallete.swift for defining color constants

---
+++ Vedge/Constants/ColorPallete.swift
@@ -0,0 +1,24 @@
+//
+//  ColorPallete.swift
+//  Vedge
+//
+//  Created by 주현명 on 10/3/24.
+//
+
+import SwiftUI
+
+struct Constants {
+    static let BackgroundElevated: Color = .black.opacity(0.1)
+    static let BackgroundWhite: Color = .white
+    static let BackgroundAccent: Color = .black.opacity(0.2)
+    static let TextVariableAlternative: Color = .black.opacity(0.45)
+    static let TextBlackAccent: Color = .black
+    static let TextBlackNeutral: Color = .black.opacity(0.6)
+    static let TextBlackDefault: Color = .black.opacity(0.75)
+    static let BackgroundDefault: Color = .white
+    static let BackgroundBorder: Color = .black.opacity(0.07)
+    static let FixedContentsAccent: Color = .black
+    static let ContentsStatusUnselected: Color = Color(red: 0.7, green: 0.7, blue: 0.74)
+    static let FixedBackgoundWhite: Color = .white
+    static let VariableBackgroundComponent: Color = .black.opacity(0.2)
+}
